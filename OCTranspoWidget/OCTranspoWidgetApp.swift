a507bff92803eb6fec929df318f235839172c1cc
Kshitij <ksmavai2005@gmail.com>
2025-12-24 22:45:47 -0500

    Initial Commit

---
+++ OCTranspoWidget/OCTranspoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCTranspoWidgetApp.swift
+//  OCTranspoWidget
+//
+//  Created by kshitij savi mavai on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct OCTranspoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
